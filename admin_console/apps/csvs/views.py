3d91f2831f115db4a4cfe52671f405e19206131a
26huang <weishuoh@gmail.com>
2021-09-01 01:10:46 -0400

    updated view to flag processed

--- admin_console/apps/csvs/views.py
+++ admin_console/apps/csvs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import CsvModelForm
+from .models import Csv
 
 
 def upload_file_view(request):
@@ -8,4 +9,14 @@ def upload_file_view(request):
     if form.is_valid():
         form.save()
         form = CsvModelForm()
+        obj = Csv.objects.get(processed=False)
+        try:
+            print('file processed')
+            obj.processed = True
+            obj.save()
+
+        except BaseException as e:
+            print(e)
+
+
     return render(request, 'csvs/upload.html', {'form': form})
