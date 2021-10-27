094003257954a3d60ff589a23d467c4aefdd30c2
26huang <weishuoh@gmail.com>
2021-10-27 00:41:40 -0400

    added script to add entry and check if record already exist

--- admin_console/apps/accounts/models.py
+++ admin_console/apps/accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Gender(models.Model):
-    gender = models.CharField(max_length=6)
+    gender = models.CharField(max_length=6, unique=True)
 
     def __str__(self):
         return '{}'.format(self.gender)
--- admin_console/scripts/insert_data.py
+++ admin_console/scripts/insert_data.py
@@ -1,9 +1,39 @@
 from apps.accounts.models import Customer, Gender
 
-g1 = Gender(gender='Male')
-g2 = Gender(gender='Female')
-g1.save()
-g2.save()
+def add_gender_lookup(gender_list):
+    for gender in gender_list:
+        try:
+            g = Gender(gender=gender)
+            g.save()
+        except BaseException as e:
+            print(e)
+
+gender_list = ['Male', 'Female']
+add_gender_lookup(gender_list)
+
 
 c1 = Customer(gender=Gender.objects.get(gender='Male'), name='Bob')
 c1.save()
+
+
+def add_customer(name, gender):
+    try:
+        if Customer.objects.filter(gender=Gender.objects.get(gender=gender), name=name).exists():
+            return 'Record already exists'
+        else:
+            gender_id = Gender.objects.get(gender=gender)
+            c = Customer(gender=gender_id, name=name)
+            c.save()
+            return 'Customer {} has been added.'.format(name)
+
+    except BaseException as e:
+        g = Gender(gender=gender)
+        g.save()
+        add_customer(name, gender)
+        return e
+
+
+add_customer('Bao', 'Female')
+add_customer('Bao', 'Unicorn')
+add_customer('Bao', 'Wizard')
+
